faff4bbc39b9e7c600a6f2907ee5956fc2c937a7
Toluwase <toluwase103@gmail.com>
2019-04-17 23:37:36 +0100

    [#165430859] Top menu and sidebar template mock developed
    A mock for the top menu and sidebar template as been developed

---
+++ UI/templates/top-menu.template.js
@@ -0,0 +1,15 @@
+const templateHtml = `
+    <div class="menu">
+        <span class="logo">Quick Credit</span>
+        <nav>
+            {{links}}
+        </nav>
+    </div>
+`;
+
+
+const topMenuTemplate = {
+    root: 'top-menu-root',
+    classNames: [],
+    template: templateHtml
+};
