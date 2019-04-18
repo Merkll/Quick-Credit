55f04e7be5f1e055cab2a9924bbd21653bb49a65
Toluwase <toluwase103@gmail.com>
2019-04-18 01:58:06 +0100

    [#165430859] Build the template Engine
    A template script was developed to handle the rendering of template/component inorder
    to encourage reusability.

--- templates/sidebar.template.js
+++ templates/sidebar.template.js
@@ -1,15 +1,18 @@
-const templateHtml = `
-    <div class="sidebar">
-        <span class="account-name">Hi, {{accountName}}</span>
-        <a href="./loans.html">Loans</a>
-        <a href="./application.html">Apply</a>
-        <a href="./inbox.html">Inbox <i>({{inboxCount}})</i></a>
-    </div>
-`;
+(function(){
+    const templateHtml = `
+        <div class="sidebar">
+            <span class="account-name">Hi, {{accountName}}</span>
+            <a href="./loans.html">Loans</a>
+            <a href="./application.html">Apply</a>
+            <a href="./inbox.html">Inbox <i>({{inboxCount}})</i></a>
+        </div>
+    `;
 
 
-const sidebarTemplate = {
-    root: 'top-menu',
-    classNames: [],
-    template: templateHtml
-}
+    const sidebarTemplate = {
+        root: 'sidebar-root',
+        classNames: [],
+        template: templateHtml
+    }
+    templates['sidebar'] = sidebarTemplate;
+}());
