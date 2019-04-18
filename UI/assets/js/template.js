92fc3ddbd846f7f6f41c8c3d02fa6ca2b0c582ce
Toluwase <toluwase103@gmail.com>
2019-04-18 21:29:24 +0100

    Fix bug in template engine to accomodate multiple child components in a component

--- UI/assets/js/template.js
+++ UI/assets/js/template.js
@@ -1,112 +1,100 @@
-/**
- * Loads the temlate files as script tags in the dom.
- *  Since we do not have access to the file system, the templatefiles have to be provided as arrays
- * @param {Array} templateFiles 
- * @param {String} templatePath
- */
 var templates = {};
-const loadTemplateFiles = (templateFiles, templatePath = '/UI/templates/') =>{
-    if(!(templateFiles instanceof Array)) return;
-    const files = templateFiles.map(template => {
+const render = (()=>{
+        
+    /**
+     * Loads the temlate files as script tags in the dom.
+     *  Since we do not have access to the file system, the templatefiles have to be provided as arrays
+     * @param {Array} templateFiles 
+     * @param {String} templatePath
+     */
+    const loadTemplateFiles = (components, templatePath = '/UI/templates/') =>{
+        if(!(components instanceof Array)) return;
+        return components.map(async (template) => await loadTemplateFile(template, templatePath));
+    }
+
+    const loadTemplateFile = (component, templatePath = '/UI/templates/') => {
+        if(templates[component]) return { component: templates[component], template: templates[component].template } ;
+        if(component.type == 'literal') return {component: component.data, template: component.data };
         return new Promise((resolve, reject) =>{
             const referenceScript = document.getElementById('template-js');
             const script = document.createElement('script');
-            script.src = `${templatePath}${template}.template.js`;
-            script.defer = false;
-            script.async= false;
+            script.src = `${templatePath}${component}.template.js`;
             document.body.insertBefore(script, referenceScript);
             script.addEventListener('load', () => {
-                resolve(template);
+                const componentObject = templates[component];
+                const template = templates[component].template;
+                resolve({ component: componentObject, template });
             });
         });
-    });
-    return Promise.all(files);
-}
-let count = 0;
-const replaceTrailingTags = (templateHtml) => {
-    const tagsRegularExp = new RegExp(/{{.*?}}/, 'gi');    
-    return templateHtml.replace(tagsRegularExp, "");
-}
-
-const replaceTag = (templateHtml, tag, value) => {
-    const templateTag = `{{${tag}}}`;
-    return templateHtml.replace(new RegExp(templateTag, 'g'), `${value}${templateTag}`);
-}
-
-const multiTagsPopulate = ({templateHtml, tags, rootTag, childTemplate, childTag}) => {
-  for (const singleTag of tags) {
-      const data = singleTag.data;
-      const baseTemplate = childTemplate || singleTag.baseTemplate;
-      const baseTag = childTag || rootTag;
-      if(data){
-          templateHtml = replaceTag(templateHtml, rootTag, data);
-      }else if(baseTemplate && templates[baseTemplate]){
-            const html = render(baseTemplate, singleTag);
-            const childTemplate = [];
-            childTemplate[baseTag] = html; 
-            templateHtml = populate(templateHtml, childTemplate); 
-            console.log(templateHtml);           
-
-      }else if(baseTemplate){
-          const baseHtml = populate(baseTemplate, singleTag);
-          templateHtml = replaceTag(templateHtml, rootTag, baseHtml)
-      }      
-
-
     }
-    return templateHtml;
-} 
 
-const isArray = (data) => !!(data instanceof Array)
-const isObject = (data) => !!(data instanceof Object)
-/**
- * 
- * @param {String} templateHtml 
- * @param {Object | Array} tags template tags to replace
- * @param {String} rootTag the root tag to replace in event of multiple fields
- */
-const populate = (templateHtml, tags, rootTag = "") => {
-    if(isObject(tags)){
-        if(tags.childrenTemplate) {
-            const childTemplate = tags.childrenTemplate;
-            const childTag = tags.childrenTag;
-            if(isArray(tags.data)){
-                templateHtml = multiTagsPopulate({templateHtml, tags:tags.data, childTemplate, childTag});
-            }
+    const replaceTrailingTags = (templateHtml) => {
+        const tagsRegularExp = new RegExp(/{{.*?}}/, 'gi');  
+        return templateHtml.replace(tagsRegularExp, "");
+    }
+
+    const replaceTag = (templateHtml, tag, value) => {
+        const templateTag = `{{${tag}}}`;
+        return templateHtml.replace(new RegExp(templateTag, 'g'), `${value}${templateTag}`);
+    }
+
+    const multipleChildPopulate = async ({ templateHtml, childNodes, childTag, childTemplate, childComponent, rootTag}) => {
+        for (const child of childNodes) {
+            const { data, baseTemplate = childComponent } = child; //data takes precedence
+            if(data){
+                templateHtml = replaceTag(templateHtml, childTag, data);
+            }else if(baseTemplate){
+                const html = await render(baseTemplate, child);
+                const childTemplate = {};
+                childTemplate[childTag] = html; 
+                templateHtml = await populate({templateHtml, tags: childTemplate}); 
+            }   
         }
-        for (let [tag, value] of Object.entries(tags)){
-            if(isArray(value)){
-                templateHtml = multiTagsPopulate({templateHtml, tags:value, rootTag:tag});
+        return templateHtml;
+    } 
 
-            }else{
-                templateHtml = replaceTag(templateHtml, tag, value);
-            }
-        } 
-    }else if(isArray(tags)){
-        templateHtml = multiTagsPopulate(templateHtml, value, rootTag);
+    const isArray = (data) => !!(data instanceof Array)
+    const isObject = (data) => !!(data instanceof Object)
+    /**
+     * 
+     * @param {String} templateHtml 
+     * @param {Object | Array} tags template tags to replace
+     * @param {String} rootTag the root tag to replace in event of multiple fields
+     */
+    const populate = async ({templateHtml, tags, rootTag = "", component = {}}) => {
+        if(!isObject(tags)) return;
+        const { childComponent: componentChildComponent, childTag: componentChildTag } = component;
+        const { childTag = componentChildTag, childNodes, childComponent = componentChildComponent } = tags;
+        if(childTag && isArray(childNodes)) {
+            templateHtml = await multipleChildPopulate({ templateHtml, childNodes, childTag, childComponent});
+        }else{
+            for (const [tag, value] of Object.entries(tags)){
+                if(isArray(value)){
+                    templateHtml = await multipleChildPopulate({templateHtml, childNodes:value, rootTag:tag});
+                }else{
+                    templateHtml = replaceTag(templateHtml, tag, value);
+                }
+            } 
+        }
+        return templateHtml.trim();
     }
-    return templateHtml;
-    return replaceTrailingTags(templateHtml);
-}
 
-/**
- * Renders the template file
- * @param {String} template 
- */
-
-const render = (template, tags) => {
-    if(templates && templates[template]){
-        template = templates[template];
-        let html = populate(template.template, tags).trim();
-        html = replaceTrailingTags(html);
-        console.log(html)
-        const templateRoot = template.root;
-        const templateRenderFunction = template.render;
-        if(templateRenderFunction) return templateRenderFunction(html);
-        const rootElement = document.getElementById(templateRoot);
-        if(rootElement) rootElement.innerHTML = html; 
-        return html;
-        
+    /**
+     * Renders the template file
+     * @param {String} template 
+     */
+    const render = async (templateComponent, tags) => {
+       const { component, template: templateHtml } = await loadTemplateFile(templateComponent);
+        if(component){
+            const populatedTemplate = await populate({templateHtml, tags, component});
+            const html = replaceTrailingTags(populatedTemplate);
+            const { root, render: templateRenderFunction } = component;
+            if(templateRenderFunction) return templateRenderFunction(html);
+            const rootElement = document.getElementById(root);
+            if(rootElement) rootElement.innerHTML = html; 
+            return html;            
+        }
     }
-}
+    return render;
+})();
 
