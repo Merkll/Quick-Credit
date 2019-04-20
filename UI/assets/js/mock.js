cec15d69eee9229437395b13b439d27c2bb5f775
Toluwase <toluwase103@gmail.com>
2019-04-20 10:01:43 +0100

    Added individual page rendering capability by using dataset to mark pages

--- UI/assets/js/mock.js
+++ UI/assets/js/mock.js
@@ -15,6 +15,241 @@ const Mock = (function(){
             ]
     };
 
+    const loans = {
+        childNodes: [
+            {
+                loanId:  '#88828289',
+                status: 'Current',
+                applicationDate: '31 Mar 2019',
+                loanAmount: 6000,
+                loanTenor: 6,
+                loanBalance: 3000,
+                nextRepayment: '31 Mar 2019'
+            },
+            {
+                loanId:  '#88828288',
+                status: 'Repaid',
+                applicationDate: '30 Mar 2019',
+                loanAmount: 6000,
+                loanTenor: 6,
+                loanBalance: 3000,
+                nextRepayment: '31 Mar 2019'
+            },
+        ]
+    }; 
+
+    const clients = {
+        childComponent: 'clients-card',
+        childTag: 'cards',
+        childNodes: [
+            {
+                userId:  '#88828289',
+                status: 'Approved',
+                userJoinedDate: '31 Mar 2019',
+                userLoans: 1,
+                userEmail: "john@example.com"
+            },
+            {
+                userId:  '#88828288',
+                status: 'Pending',
+                userJoinedDate: '31 Mar 2019',
+                userLoans: 1,
+                userEmail: "mike@example.com"
+            },
+        ]
+    };
+
+    const clientDetails = {
+        '#88828289': {
+            userId:  '#88828289',
+            status: 'Approved',
+            userJoinedDate: '31 Mar 2019',
+            userLoans: 1,
+            userEmail: "john@example.com",
+            userFirstName: "Mark",
+            userLastName: "Paul",
+            userMobile: "081324590789",
+            userAddress: "lekki",
+            action: {
+                childTag: 'action',
+                childComponent: {type: 'literal', data:' <button class="btn float-right overlay-btn client-action" data-action="{{buttonAction}}">{{text}}</button>'},
+                childNodes: [
+                        {
+                            text: "Approve",
+                            buttonAction: 'approve'
+                        },
+                        {
+                            text: "Reject",
+                            buttonAction: 'approve'
+                        }
+                    ]
+            }
+        },
+        '#88828288': {
+            userId:  '#88828288',
+            status: 'Pending',
+            userJoinedDate: '31 Mar 2019',
+            userLoans: 1,
+            userEmail: "mike@example.com",
+            userFirstName: "Mike",
+            userLastName: "John",
+            userMobile: "081324455789",
+            userAddress: "lekki"
+        },
+    };
+
+    const adminSidebar = {
+        "accountName": "Mike John",
+        "inboxCount": 1,
+        "links": {
+            childTag: 'links',
+            childComponent: {type: 'literal', data:'<a href="{{href}}">{{text}}</a>'},
+            childNodes: [
+                    {
+                        text: "Loans",
+                        href: './loans.html',
+                    },
+                    {
+                        text: "Clients",
+                        href: './clients.html',
+                    },
+                    {
+                        text: "Repayments",
+                        href: './repayments',
+                    }
+                ]
+        }
+    };
+
+    const clientSidebar = {
+        "accountName": "Mike John",
+        "inboxCount": 1,
+        "links": {
+            childTag: 'links',
+            childComponent: {type: 'literal', data:'<a href="{{href}}">{{text}}</a>'},
+            childNodes: [
+                    {
+                        text: "Loans",
+                        href: './loans.html',
+                    },
+                    {
+                        text: "Apply",
+                        href: './application.html',
+                    }
+                ]
+        }
+    };
+
+    const message = {
+        categories: {
+            childNodes: [
+                {
+                    text: 'New',
+                    class: 'new-message active-category',
+                    linkClass: 'new-message'
+                },
+                {
+                    text: 'Loan-#88iodha98',
+                    linkClass: 'view-message',
+                    messageId: '#88828289',
+                }
+            ]
+        },
+        details: {
+            "#88828289": {
+                subject: "Approval of Loan #9908udu",
+                content : "Loan Approved",
+            }
+        }
+    };
+    const clientTopMenu = {
+        childTag: 'links',
+        childComponent: {type: 'literal', data:'<a href="{{href}}">{{text}}</a>'},
+        childNodes: [
+                {
+                    text: "Logout",
+                    href: './home.html',
+                }
+            ]
+    };
+
+    const repayments = {
+        "#88828288" : {
+                    childNodes: [
+                        {
+                            paymentId: '#9i998746564',
+                            paymentDate: '31 March 2019',
+                            paymentAmount: 6000,
+                            balance: 6000
+                        },
+                        {
+                            paymentId: '#9i998746564',
+                            paymentDate: '31 March 2019',
+                            paymentAmount: 6000,
+                            balance: 6000
+                        }
+                    ]
+                },
+        "#88828289" : {
+                    childNodes: [
+                        {
+                            paymentId: '#9i998746564',
+                            paymentDate: '31 March 2019',
+                            paymentAmount: 6000,
+                            balance: 6000
+                        }
+                    ]
+                }
+        
+    };
+
+    const loanDetails = {
+        "#88828288" : {
+            loanId:  '#88828288',
+            status: 'Current',
+            applicationDate: '30 Mar 2019',
+            loanAmount: 6000,
+            loanTenor: 6,
+            loanBalance: 3000,
+            nextRepayment: '31 Mar 2019',
+            action: {
+                childTag: 'action',
+                childComponent: {type: 'literal', data:' <button class="btn float-right overlay-btn loan-action" data-action="{{buttonAction}}">{{text}}</button>'},
+                childNodes: [
+                        {
+                            text: "Post Repayment",
+                            buttonAction: 'Repayment'
+                        }
+                    ]
+            }
+        },
+        "#88828289" : {
+            loanId:  '#88828289',
+            status: 'Pending Approval ',
+            applicationDate: '31 Mar 2019',
+            loanAmount: 6000,
+            loanTenor: 6,
+            loanBalance: 3000,
+            nextRepayment: '31 Mar 2019',
+            action: {
+                childTag: 'action',
+                childComponent: {type: 'literal', data:' <button class="btn float-right overlay-btn loan-action" data-action="{{buttonAction}}">{{text}}</button>'},
+                childNodes: [
+                        {
+                            text: "Approve",
+                            buttonAction: 'Approval',
+                        },
+                        {
+                            text: "Reject",
+                            buttonAction: 'Rejection',
+                        }
+                    ]
+            }            
+        },
+    };
+
+    
+
 
 
 
@@ -160,122 +395,49 @@ const Mock = (function(){
         return (getPage() == 'clients.html') ? clients : loan;
     });
 
-    const repayments = {
-        "#88828288" : {
-                    childNodes: [
-                        {
-                            paymentId: '#9i998746564',
-                            paymentDate: '31 March 2019',
-                            paymentAmount: 6000,
-                            balance: 6000
-                        },
-                        {
-                            paymentId: '#9i998746564',
-                            paymentDate: '31 March 2019',
-                            paymentAmount: 6000,
-                            balance: 6000
-                        }
-                    ]
-                },
-        "#88828289" : {
-                    childNodes: [
-                        {
-                            paymentId: '#9i998746564',
-                            paymentDate: '31 March 2019',
-                            paymentAmount: 6000,
-                            balance: 6000
-                        }
-                    ]
-                }
-        
-    }
+   
 
-    const loanDetails = {
-        "#88828288" : {
-            loanId:  '#88828288',
-            status: 'Current',
-            applicationDate: '30 Mar 2019',
-            loanAmount: 6000,
-            loanTenor: 6,
-            loanBalance: 3000,
-            nextRepayment: '31 Mar 2019',
-            action: {
-                childTag: 'action',
-                childComponent: {type: 'literal', data:' <button class="btn float-right overlay-btn loan-action" data-action="{{buttonAction}}">{{text}}</button>'},
-                childNodes: [
-                        {
-                            text: "Post Repayment",
-                            buttonAction: 'Repayment'
-                        }
-                    ]
-            }
-        },
-        "#88828289" : {
-            loanId:  '#88828289',
-            status: 'Pending Approval ',
-            applicationDate: '31 Mar 2019',
-            loanAmount: 6000,
-            loanTenor: 6,
-            loanBalance: 3000,
-            nextRepayment: '31 Mar 2019',
-            action: {
-                childTag: 'action',
-                childComponent: {type: 'literal', data:' <button class="btn float-right overlay-btn loan-action" data-action="{{buttonAction}}">{{text}}</button>'},
-                childNodes: [
-                        {
-                            text: "Approve",
-                            buttonAction: 'Approval',
-                        },
-                        {
-                            text: "Reject",
-                            buttonAction: 'Rejection',
-                        }
-                    ]
-            }            
-        },
-    }
-
-    const clients = {
-        '#88828289': {
-            userId:  '#88828289',
-            status: 'Approved',
-            userJoinedDate: '31 Mar 2019',
-            userLoans: 1,
-            userEmail: "john@example.com",
-            userFirstName: "Mark",
-            userLastName: "Paul",
-            userMobile: "081324590789",
-            userAddress: "lekki",
-            action: {
-                childTag: 'action',
-                childComponent: {type: 'literal', data:' <button class="btn float-right overlay-btn client-action" data-action="{{buttonAction}}">{{text}}</button>'},
-                childNodes: [
-                        {
-                            text: "Approve",
-                            buttonAction: 'approve'
-                        },
-                        {
-                            text: "Reject",
-                            buttonAction: 'approve'
-                        }
-                    ]
-            }
-        },
-        '#88828288': {
-            userId:  '#88828288',
-            status: 'Pending',
-            userJoinedDate: '31 Mar 2019',
-            userLoans: 1,
-            userEmail: "mike@example.com",
-            userFirstName: "Mike",
-            userLastName: "John",
-            userMobile: "081324455789",
-            userAddress: "lekki"
-        },
-    }
+    // const clientDetails = {
+    //     '#88828289': {
+    //         userId:  '#88828289',
+    //         status: 'Approved',
+    //         userJoinedDate: '31 Mar 2019',
+    //         userLoans: 1,
+    //         userEmail: "john@example.com",
+    //         userFirstName: "Mark",
+    //         userLastName: "Paul",
+    //         userMobile: "081324590789",
+    //         userAddress: "lekki",
+    //         action: {
+    //             childTag: 'action',
+    //             childComponent: {type: 'literal', data:' <button class="btn float-right overlay-btn client-action" data-action="{{buttonAction}}">{{text}}</button>'},
+    //             childNodes: [
+    //                     {
+    //                         text: "Approve",
+    //                         buttonAction: 'approve'
+    //                     },
+    //                     {
+    //                         text: "Reject",
+    //                         buttonAction: 'approve'
+    //                     }
+    //                 ]
+    //         }
+    //     },
+    //     '#88828288': {
+    //         userId:  '#88828288',
+    //         status: 'Pending',
+    //         userJoinedDate: '31 Mar 2019',
+    //         userLoans: 1,
+    //         userEmail: "mike@example.com",
+    //         userFirstName: "Mike",
+    //         userLastName: "John",
+    //         userMobile: "081324455789",
+    //         userAddress: "lekki"
+    //     },
+    // }
     
 
-    const messageCategoryDetails = {
+    const messageDetails = {
         "#88828289": {
             subject: "Approval of Loan #9908udu",
             content : "Loan Approved",
@@ -299,23 +461,33 @@ const Mock = (function(){
 
     return {
             mock: () => {
-                render('sidebar', sideBar);
-                render('top-menu', topMenu);
-                render('card-container', cardContainer());
-                render('message', messageCategoryDetails);
-                render('message-category', messageCategory );
-                render('message-single-category', messageCategoryDetails);
-                render('single-loan', {}, {repayments: repayments["#88828289"],
-                    loanDetails: loanDetails["#88828289"]});
+                // render('sidebar', sideBar);
+                // render('top-menu', topMenu);
+                // render('card-container', cardContainer());
+                // render('message', messageCategoryDetails);
+                // render('message-category', messageCategory );
+                // render('message-single-category', messageCategoryDetails);
+                // render('single-loan', {}, {repayments: repayments["#88828289"],
+                //     loanDetails: loanDetails["#88828289"]});
                 render('single-client', {}, { clientDetails: clients["#88828289"],
                     clientLoans: cardContainer(true)});
             },
+            HomeTopMenu,
+            loans,
+            adminSidebar,
+            clientSidebar,
+            message,
+            clientTopMenu,
+            repayments,
+            loanDetails,
+            clientDetails,
+            clients,
             data: {
                 repayments,
                 loanDetails,
                 clients,
                 cardContainer,
-                message: messageCategoryDetails,
+                // message: messageCategoryDetails,
                 HomeTopMenu,
             }
         }
