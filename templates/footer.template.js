62bb2b47b2bc456d7d7ba9ed219c36aada722904
Toluwase <toluwase103@gmail.com>
2019-04-22 22:11:34 +0100

    #165516139 Created site footer template

---
+++ templates/footer.template.js
@@ -0,0 +1,15 @@
+(function(){
+    const templateHtml = `
+        <footer class="footer">
+                <small>&copy; Copyright 2018,Quick Credit</small>   
+        </footer>
+    `;
+
+
+    const footer = {
+        root: 'footer-root',
+        classNames: [],
+        template: templateHtml
+    };
+    templates['footer'] = footer;
+}());
