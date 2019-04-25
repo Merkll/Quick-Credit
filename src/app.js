7cb5f2f2ca229296a8bf60d5a7fa5428187efeb9
Toluwase <toluwase103@gmail.com>
2019-04-25 16:44:58 +0100

    Added an express sample app to test deployment to heroku

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
