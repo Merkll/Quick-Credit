528128c9ada1380c7b67987b50511e6faf83dd43
Toluwase <toluwase103@gmail.com>
2019-04-25 17:15:01 +0100

    Fix typo in access the environment port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
