dbcfeac660dfc12194765bf90b50702ab7550c12
Toluwase <toluwase103@gmail.com>
2019-04-30 12:55:50 +0100

    [#165714922] Build User service that handles all  actions capable by the user entity
    These actions are:
    1. user verification
    2. specific user query
    3. All users query
    4. verified users query
    5. unverified users query

---
+++ src/services/user.js
@@ -0,0 +1,15 @@
+const { User } = require('../model');
+
+exports.verify = (email) => {
+  if (!email) throw new Error('Email is undefined');
+  return User.update({ status: 'verified' }, { email }).data[0];
+};
+
+exports.getUser = (email) => {
+  if (!email) throw new Error('Email is undefined');
+  return User.find({ email }).data[0];
+};
+
+exports.getAllUsers = () => User.findAll().data;
+exports.getVerifiedUsers = () => User.find({ status: 'verified' }).data;
+exports.getUnverifiedUsers = () => User.find({ status: 'unverified' }).data;
---
+++ test/user-service.spec.js
@@ -0,0 +1,107 @@
+/* eslint-disable no-unused-expressions */
+const { expect } = require('chai');
+const faker = require('faker');
+const {
+  verify,
+  getUser,
+  getAllUsers,
+  getUnverifiedUsers,
+  getVerifiedUsers,
+} = require('../src/services/user');
+const { User } = require('../src/model');
+
+describe('User Service', () => {
+  context('verify', () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    before(() => {
+      User.insert({ email, password });
+    });
+    it('Should throw error if email is undefined', () => {
+      expect(() => verify()).to.throw();
+    });
+    it('Should return object when provided mail', () => {
+      const data = verify(email);
+      expect(data).to.be.an.instanceof(Object);
+      expect(data.email).to.be.eql(email);
+    });
+  });
+
+  context('getUser', () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    before(() => {
+      User.insert({ email, password });
+    });
+    it('Should throw error if email is undefined', () => {
+      expect(() => getUser()).to.throw();
+    });
+    it('Should return object when provided mail', () => {
+      const data = getUser(email);
+      expect(data).to.be.an.instanceof(Object);
+      expect(data.email).to.be.eql(email);
+    });
+  });
+
+  context('getAllUsers', () => {
+    before(() => {
+      const UserData = Array(10).fill(0).map((data, index) => ({
+        id: index + 1,
+        email: faker.internet.email(),
+        firstName: faker.name.findName(),
+        lastName: faker.name.lastName(),
+        password: faker.random.uuid(),
+        address: faker.address.streetAddress(),
+        status: 'unverified',
+        isAdmin: faker.random.boolean(),
+      }
+      ));
+      User.insert(UserData);
+    });
+    it('Should return an array of object', () => {
+      const data = getAllUsers();
+      expect(data).to.be.an.instanceof(Array);
+    });
+  });
+  context('getAllUsers', () => {
+    before(() => {
+      const UserData = Array(10).fill(0).map((data, index) => ({
+        id: index + 1,
+        email: faker.internet.email(),
+        firstName: faker.name.findName(),
+        lastName: faker.name.lastName(),
+        password: faker.random.uuid(),
+        address: faker.address.streetAddress(),
+        status: 'unverified',
+        isAdmin: faker.random.boolean(),
+      }
+      ));
+      User.insert(UserData);
+    });
+    it('Should return an array of object', () => {
+      const data = getUnverifiedUsers();
+      expect(data).to.be.an.instanceof(Array);
+    });
+  });
+
+  context('getVerifiedUsers', () => {
+    before(() => {
+      const UserData = Array(10).fill(0).map((data, index) => ({
+        id: index + 1,
+        email: faker.internet.email(),
+        firstName: faker.name.findName(),
+        lastName: faker.name.lastName(),
+        password: faker.random.uuid(),
+        address: faker.address.streetAddress(),
+        status: 'verified',
+        isAdmin: faker.random.boolean(),
+      }
+      ));
+      User.insert(UserData);
+    });
+    it('Should return an array of object', () => {
+      const data = getVerifiedUsers();
+      expect(data).to.be.an.instanceof(Array);
+    });
+  });
+});
