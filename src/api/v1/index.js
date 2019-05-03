90c28d1315eff5ae7a9da8b8afc386e44d7d46ff
Toluwase <toluwase103@gmail.com>
2019-05-03 11:14:19 +0100

    feature(user-verification): build API endpoint to mark user as verified
    create an api endpoint required for admin to mark existing users as verified
    
    [finishes #165558741]

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { MethodNotAllowedError } = require('../../lib/error');
 const { signup, signin } = require('./controllers/auth');
+const { verify } = require('./controllers/user');
 
 router.route('/auth/signup')
   .post(signup)
@@ -10,6 +11,12 @@ router.route('/auth/signup')
 
 router.route('/auth/signin')
   .post(signin)
+  .all(() => {
+    throw new MethodNotAllowedError();
+  });
+
+router.route('/user/:email/verify')
+  .patch(verify)
   .all(() => {
     throw new MethodNotAllowedError();
   });
