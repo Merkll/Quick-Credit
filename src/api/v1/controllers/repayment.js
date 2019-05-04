73a0da384088bdbffb99fa46da946c0f8b7da41f
Toluwase <toluwase103@gmail.com>
2019-05-04 20:11:51 +0100

    feature(loan-approval): build api endpoint to post repayment for a loan aplication
     create an api endpoint required for admins to create loan repayment in favour of a client
    
    [finishes #165558872]

--- src/api/v1/controllers/repayment.js
+++ src/api/v1/controllers/repayment.js
@@ -1,15 +1,31 @@
 const {
   NotFoundError,
+  InvalidRequestBodyError,
 } = require('../../../lib/error');
 const {
   getLoanRepayments,
+  createRepayment,
 } = require('../../../services/repayment');
+
+const {
+  getLoan,
+} = require('../../../services/loan');
+
 const Response = require('../../../lib/response');
 
 exports.getLoanRepayments = (req, res) => {
   const { loan } = req.params;
   const data = getLoanRepayments(loan);
   if (data.length === 0) throw new NotFoundError('No repayment for that loan found');
+  const response = new Response(data);
+  res.status(response.status).send(response);
+};
+
+exports.postLoanRepayment = (req, res) => {
+  const { loan } = req.params;
+  const loanDetails = getLoan(loan);
+  if (!loanDetails) throw new NotFoundError('loan with that id not found');
+  const data = createRepayment(loan);
   const response = new Response(data);
   res.status(response.status).send(response);
 };
