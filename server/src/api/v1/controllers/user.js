97cb6c2d7e0c613ed9fda4b302f5e882ce2d2d38
Toluwase <toluwase103@gmail.com>
2019-05-06 11:08:42 +0100

    feature(view-user): build api endpoint to view details of a specific user
    [finishes #165819129]

--- server/src/api/v1/controllers/user.js
+++ server/src/api/v1/controllers/user.js
@@ -2,7 +2,10 @@
 const {
   NotFoundError,
 } = require('../../../lib/error');
-const { verify, getAllUsers, filterUsers } = require('../../../services/user');
+const { 
+  verify, 
+  getAllUsers, filterUsers, getUser, 
+} = require('../../../services/user');
 const Response = require('../../../lib/response');
 
 exports.verify = (req, res) => {
@@ -19,6 +22,14 @@ exports.getUsers = (req, res) => {
   let data;
   if (statusEnums.includes(status)) data = filterUsers({ status });
   else data = getAllUsers();
+  const response = new Response(data);
+  res.status(response.status).json(response);
+};
+
+exports.getUser = (req, res) => {
+  const { email } = req.params;
+  const data = getUser(email);
+  if (!data) throw new NotFoundError('User with that email address doesnt exist');
   const response = new Response(data);
   res.status(response.status).json(response);
 };
