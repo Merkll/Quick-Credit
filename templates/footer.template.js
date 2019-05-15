1e547bc4e644a1b6234328ddefa3d8ae66ee1b2f
Toluwase <toluwase103@gmail.com>
2019-05-15 17:03:53 +0100

    user-interface-design: redesign the password reset page and general color schemes

--- templates/footer.template.js
+++ templates/footer.template.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-param-reassign */
 ((global) => {
   const templateHtml = `
-    <footer class="footer">
-            <small>&copy; Copyright 2018,Quick Credit</small>   
-    </footer>
+   
 `;
 
 
