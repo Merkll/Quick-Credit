aa32f1264493fa43c7ee71ed3e71235cf1b85abb
Toluwase <toluwase103@gmail.com>
2019-05-29 07:45:09 +0100

    fix issue with error handler

--- server/src/middleware/error-handler.js
+++ server/src/middleware/error-handler.js
@@ -1,9 +1,9 @@
 import { MethodNotAllowedError } from '../helpers/error';
 
-export const ErrorHandler = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+export const ErrorHandler = (err, req, res, next) => {
   if (err.status) res.status(err.status).send(err);
   else res.status(500).send(err);
-  // next(err);
 };
 
 export const MethodNotAllowed = (req, res, next) => {
